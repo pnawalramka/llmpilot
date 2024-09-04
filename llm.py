2a27c2f935097c90713d7799938aea14c1d07cd0
Priyanka Nawalramka <priyanka.nawalramka@gmail.com>
2024-09-04 08:29:44 -0600

    update models

--- llm.py
+++ llm.py
@@ -24,26 +24,57 @@ class LLMPilot:
             if not model_choice or model_choice == 'gpt4':
                 print('llm = gpt4')
                 self._llm = ChatOpenAI(
-                    model='gpt-4-turbo', temperature=0.7, api_key=openai_api_key)
+                    model='gpt-4o-mini', temperature=0.7, api_key=openai_api_key)
             elif model_choice == 'gpt3':
                 print('llm = gpt3')
-                self._llm = OpenAI(
-                    model='gpt-3.5-turbo-instruct', temperature=0.7, api_key=openai_api_key)
+                self._llm = ChatOpenAI(
+                    model='gpt-3.5-turbo', temperature=0.7, api_key=openai_api_key)
             elif model_choice == 'claude':
                 print('llm = claude2')
                 session = boto3.Session()
                 bedrock_client = session.client(
                     service_name='bedrock',
                     region_name='us-west-2',
+                    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
                 )
                 # output_text = bedrock.list_foundation_models()
                 # print(json.dumps(output_text, indent=4))
-
                 self._llm = BedrockLLM(
                     region_name='us-west-2',
                     model_id='anthropic.claude-v2',
                     model_kwargs={
                     'max_tokens_to_sample': 512,
+                    'temperature': 0.7,
+                })
+            elif model_choice == 'llama':
+                print('llm = llama')
+                session = boto3.Session()
+                bedrock_client = session.client(
+                    service_name='bedrock',
+                    region_name='us-west-2',
+                    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+                )
+                self._llm = BedrockLLM(
+                    region_name='us-west-2',
+                    model_id='meta.llama3-1-70b-instruct-v1:0',
+                    model_kwargs={
+                    'temperature': 0.7,
+                })
+            elif model_choice == 'mistral':
+                print('llm = mistral')
+                session = boto3.Session()
+                bedrock_client = session.client(
+                    service_name='bedrock',
+                    region_name='us-west-2',
+                    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+                    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+                )
+                self._llm = BedrockLLM(
+                    region_name='us-west-2',
+                    model_id='mistral.mistral-large-2407-v1:0',
+                    model_kwargs={
                     'temperature': 0.7,
                 })
         return self._llm
@@ -56,8 +87,9 @@ class LLMPilot:
 
 
     def query_db(self, query_text):
-        # query_text = f'{query_text} Generate SQL without pre-amble.'
-        query_text = f'{query_text}'
+        # query_text = f'{query_text} Generate the syntactically correct SQL query ready to execute, without preamble or enclosing backticks or markdown.'
+        query_text = f'{query_text} Remove the preamble or markdown before executing the query.'
+        # query_text = f'{query_text}'
         db = SQLDatabase.from_uri(db_path)
         chain = SQLDatabaseChain.from_llm(self.llm, db, return_direct=True, verbose=True)
         llm_res = chain.invoke(query_text)
